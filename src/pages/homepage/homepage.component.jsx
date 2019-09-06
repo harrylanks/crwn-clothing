6aa1efc12d197b966bd73da50eec3a3fdfcdb953
Harish Lanka <harrylanka@gmail.com>
2019-09-06 17:09:19 +0530

    added styles and menu items using components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepage.styles.scss";
+import "../../components/directory/directory.component";
+import Directory from "../../components/directory/directory.component";
+
+export const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
